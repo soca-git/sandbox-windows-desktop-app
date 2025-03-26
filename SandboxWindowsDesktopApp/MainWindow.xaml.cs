7fb951e45c76aee7ce6065d488f348a4dd151f8a
soca-git <soca.mailbox@gmail.com>
2025-03-26 14:56:00 +0000

    simple BlazorWebView embedded WPF component desktop application

---
+++ SandboxWindowsDesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace SandboxWindowsDesktopApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        ServiceCollection serviceCollection = new();
+        serviceCollection.AddWpfBlazorWebView();
+
+        ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+        Resources.Add("services", serviceProvider);
+    }
+}
